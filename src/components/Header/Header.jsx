fb955af6de7fc17462f3d7306ae64758b8a6ee36
Suprio-Das <priyamd507.gmail.com>
2024-12-24 23:46:53 +0600

    Navbar title changed(Forgotten)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
                         <li><NavLink to={'/about'}>About</NavLink></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">BooksZone</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
